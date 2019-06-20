5cb26731f52ac0080673c919034cadf3113675c6
admin <rtwallace23@gmail.com>
2019-06-20 19:34:26 -0400

    generated api key. created api call for recipes.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Recipe from './Recipe';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = "675e29ed";
+  const APP_KEY = "df521867517c126cbf691daf83724b9b";
+
+  const [recipes, setRecipes] = useState([]);
+
+  //on load effect which is similar to "componenetDidMount" etc
+  useEffect(() => {
+    getRecipes();
+    }, 
+    []);
+
+    // request from the external api
+    const getRecipes = async () => {
+      const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+      const data = await response.json();
+      setRecipes(data.hits);
+    };
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className="search-form">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>       
+      </form>
+      { recipes.map(recipe => (
+        <Recipe />
+      ))}
     </div>
   );
-}
+};
 
 export default App;
