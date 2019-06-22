108919cfc30fc869fed476673c5d0ef60d02533d
admin <rtwallace23@gmail.com>
2019-06-22 12:24:28 -0400

    stylized page and individual recipe cards.

--- src/App.js
+++ src/App.js
@@ -23,14 +23,14 @@ const App = () => {
         );
       const data = await response.json();
       setRecipes(data.hits);
-      console.log(data.hits);
+      
     };
 
 const updateSearch = e => {
   setSearch(e.target.value);
-  console.log(search);
 }
 
+//prevent page reload on search & reset search field to empty
 const getSearch = e => {
   e.preventDefault();
   setQuery(search);
@@ -40,17 +40,25 @@ const getSearch = e => {
   return(
     <div className="App">
       <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
-        <button className="search-button" type="submit">Search</button>       
+        <input className="search-bar" 
+        type="text" value={search} 
+        onChange={updateSearch}
+        />
+        <button className="search-button" type="submit">
+        Search
+        </button>       
       </form>
+      <div className="recipes">
       {recipes.map(recipe => (
         <Recipe  
         key={recipe.recipe.label}
         title={recipe.recipe.label} 
         calories={recipe.recipe.calories} 
         image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}
         />
       ))}
+      </div>
     </div>
   );
 };
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,13 +1,19 @@
 import React from 'react';
+import style from './recipe.module.css'
 
-const Recipe = ({title,calories,image}) => {
+const Recipe = ({title, calories, image, ingredients}) => {
     return (
-        <div>
+        <div className = {style.recipe} >
             <h1>{title}</h1>
-            <p>{calories}</p>
-            <img src={image} alt=""></img>
+            <ol>
+                {ingredients.map(ingredients => (
+                    <li>{ingredients.text}</li>
+                ))}
+            </ol>
+            <p>Calories: {Math.floor(calories)} </p>
+            <img className={style.image} src={image} alt="" />
         </div>
     );
-}
+};
 
 export default Recipe;
