e19fa5765459b8c726bbc854da3da8ad7514631e
admin <rtwallace23@gmail.com>
2019-06-21 23:17:41 -0400

    created search states. added food information layout.

--- src/App.js
+++ src/App.js
@@ -8,28 +8,48 @@ const App = () => {
   const APP_KEY = "df521867517c126cbf691daf83724b9b";
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
   //on load effect which is similar to "componenetDidMount" etc
   useEffect(() => {
     getRecipes();
-    }, 
-    []);
+    }, [query]);
 
     // request from the external api
     const getRecipes = async () => {
-      const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+      const response = await fetch(
+        `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
+        );
       const data = await response.json();
       setRecipes(data.hits);
+      console.log(data.hits);
     };
+
+const updateSearch = e => {
+  setSearch(e.target.value);
+  console.log(search);
+}
+
+const getSearch = e => {
+  e.preventDefault();
+  setQuery(search);
+  setSearch("");
+}
 
   return(
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="text"/>
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
         <button className="search-button" type="submit">Search</button>       
       </form>
-      { recipes.map(recipe => (
-        <Recipe />
+      {recipes.map(recipe => (
+        <Recipe  
+        key={recipe.recipe.label}
+        title={recipe.recipe.label} 
+        calories={recipe.recipe.calories} 
+        image={recipe.recipe.image}
+        />
       ))}
     </div>
   );
